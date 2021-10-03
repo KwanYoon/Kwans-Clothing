63cbc83bccee63947cfbcfb60ddcff6a8d313e1f
Kwan Yoon <kwanyoon2015@gmail.com>
2021-10-03 18:02:01 -0400

    updated routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
+import './App.css';
+
+const HatsPage = () => (
+  <div>HATS PAGE</div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
