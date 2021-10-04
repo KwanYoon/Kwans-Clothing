a323ef76aef25ae1134f030b791d96e247afd1c3
Kwan Yoon <kwanyoon2015@gmail.com>
2021-10-04 15:05:06 -0400

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component.jsx';
 import './App.css';
-
-const HatsPage = () => (
-  <div>HATS PAGE</div>
-)
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
