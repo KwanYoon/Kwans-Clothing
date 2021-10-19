b020d1798f4e4b20b34ce6cc4bb8df7ecb8eefca
Kwan Yoon <kwanyoon2015@gmail.com>
2021-10-19 00:48:57 -0400

    additional features

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -10,4 +10,18 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   }
 
   return [...cartItems, {...cartItemToAdd, quantity: 1}]; // adding new cart item to array
-}
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id);
+
+  if (existingCartItem.quantity === 1) { // if there is only one in the cart, remove it entirely
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map(
+    cartItem => cartItem.id === cartItemToRemove.id
+      ? {...cartItem, quantity: cartItem.quantity - 1} // subtract from quantity if matching id
+      : cartItem
+  );
+};
